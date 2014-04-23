dc5de153baea8d8364ba0e4322a7dd6a4b9a68cb
Josh Elser <elserj@apache.org>
2014-04-23 13:22:56 -0400

    ACCUMULO-2574 Ensure the StatusCombiner is set on the replication table
    Conflicts:
    	core/src/main/java/org/apache/accumulo/core/replication/ReplicationTable.java
    	server/base/src/test/java/org/apache/accumulo/server/replication/ReplicationTableTest.java

--- core/src/main/java/org/apache/accumulo/core/replication/ReplicationTable.java
+++ core/src/main/java/org/apache/accumulo/core/replication/ReplicationTable.java
@@ -25,6 +25,8 @@ import org.apache.accumulo.core.client.Scanner;
 import org.apache.accumulo.core.client.TableExistsException;
 import org.apache.accumulo.core.client.TableNotFoundException;
 import org.apache.accumulo.core.client.admin.TableOperations;
+import org.apache.accumulo.core.iterators.Combiner;
+import org.apache.accumulo.core.iterators.IteratorUtil.IteratorScope;
 import org.apache.accumulo.core.security.Authorizations;
 import org.apache.accumulo.fate.util.UtilWaitThread;
 import org.apache.log4j.Logger;
@@ -53,6 +55,35 @@ public class ReplicationTable {
       log.error("Retrying table creation in 1 second...");
       UtilWaitThread.sleep(1000);
     }
+  }
+
+  /**
+   * Attempts to configure the replication table, will return false if it fails
+   * @param tops TableOperations for the instance
+   * @return True if the replication table is properly configured
+   */
+  protected static synchronized boolean configure(TableOperations tops) {
+    Map<String,EnumSet<IteratorScope>> iterators = null;
+    try {
+      iterators = tops.listIterators(NAME);
+    } catch (AccumuloSecurityException|AccumuloException|TableNotFoundException e) {
+      log.error("Could not fetch iterators for " + NAME, e);
+      return false;
+    }
+
+    if (!iterators.containsKey(COMBINER_NAME)) {
+      // Set our combiner and combine all columns
+      IteratorSetting setting = new IteratorSetting(50, COMBINER_NAME, StatusCombiner.class);
+      Combiner.setCombineAllColumns(setting, true);
+      try {
+        tops.attachIterator(NAME, setting);
+      } catch (AccumuloSecurityException | AccumuloException | TableNotFoundException e) {
+        log.error("Could not set StatusCombiner on replication table", e);
+        return false;
+      }
+    }
+
+    return true;
   }
 
   public static Scanner getScanner(Connector conn, Authorizations auths) throws TableNotFoundException {
