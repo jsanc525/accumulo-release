fb0f3ed244db588cef82c4cc446409696457b6ce
Josh Elser <elserj@apache.org>
2014-04-27 00:33:47 -0400

    ACCUMULO-2575 More testing to try to get status records created
    Test isn't 100% as it outlined some more deficiencies with potential
    deadlock. Will address with specific test cases rather than what
    ReplicationWithGCIT is trying to test.

--- core/src/main/java/org/apache/accumulo/core/replication/StatusUtil.java
+++ core/src/main/java/org/apache/accumulo/core/replication/StatusUtil.java
@@ -156,7 +156,7 @@ public class StatusUtil {
     if (status.getInfiniteEnd()) {
       return Long.MAX_VALUE == status.getBegin();
     } else {
-      return status.getBegin() == status.getEnd();
+      return status.getBegin() >= status.getEnd();
     }
   }
 }
