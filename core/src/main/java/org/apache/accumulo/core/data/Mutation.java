b92f7e359d3dbbf67abc8e628d1ae0a57f5b2117
Josh Elser <elserj@apache.org>
2014-06-04 13:52:58 -0400

    ACCUMULO-378 Core review fixes from bhavanki for replication

--- core/src/main/java/org/apache/accumulo/core/data/Mutation.java
+++ core/src/main/java/org/apache/accumulo/core/data/Mutation.java
@@ -485,6 +485,9 @@ public class Mutation implements Writable {
    * @return An unmodifiable view of the replication sources
    */
   public Set<String> getReplicationSources() {
+    if (null == replicationSources) {
+      return EMPTY;
+    }
     return Collections.unmodifiableSet(replicationSources);
   }
   
@@ -627,9 +630,13 @@ public class Mutation implements Writable {
       }
     }
     if (0x02 == (0x02 & hasValues)) {
-      WritableUtils.writeVInt(out, replicationSources.size());
-      for (String source : replicationSources) {
-        WritableUtils.writeString(out, source);
+      if (null == replicationSources) {
+        WritableUtils.writeVInt(out, 0);
+      } else {
+        WritableUtils.writeVInt(out, replicationSources.size());
+        for (String source : replicationSources) {
+          WritableUtils.writeString(out, source);
+        }
       }
     }
   }
