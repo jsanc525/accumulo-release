apache/accumulo

ACCUMULO-4060 Handle uncaught exceptions in FATE repo runner threads.
Any RuntimeException propagated from the internals of the FATE
repo-runner threads will caught the thread to exit. This creates
a situation where the Master can be running but be unresponsive
to any pending FATE operations like creating a table.

Make sure that these exceptions are caught and suppressed to ensure
that FATE ops can always be handled while the master is running.

fate/src/main/java/org/apache/accumulo/fate/Fate.java
@@ -38,6 +38,7 @@ public class Fate<T> {
   private static final String RETURN_PROP = "return";
 
   final private static Logger log = Logger.getLogger(Fate.class);
+  private final Logger runnerLog = Logger.getLogger(TransactionRunner.class);
 
   private TStore<T> store;
   private T environment;
@@ -91,6 +92,8 @@ public class Fate<T> {
               }
             }
           }
+        } catch (Exception e) {
+          runnerLog.error("Uncaught exception in FATE runner thread.", e);
         } finally {
           store.unreserve(tid, deferTime);
         }

jsanc525/accumulo-release

Conflicts:
	fate/src/main/java/org/apache/accumulo/fate/Fate.java

fate/src/main/java/org/apache/accumulo/fate/Fate.java
@@ -42,6 +42,7 @@ public class Fate<T> {
   private static final String RETURN_PROP = "return";
 
   final private static Logger log = LoggerFactory.getLogger(Fate.class);
+  private final Logger runnerLog = LoggerFactory.getLogger(TransactionRunner.class);
 
   private TStore<T> store;
   private T environment;
@@ -96,6 +97,8 @@ public class Fate<T> {
               }
             }
           }
+        } catch (Exception e) {
+          runnerLog.error("Uncaught exception in FATE runner thread.", e);
         } finally {
           store.unreserve(tid, deferTime);
         }