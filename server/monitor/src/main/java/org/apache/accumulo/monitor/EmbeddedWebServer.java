b3421af9999fdbc574872dfcff338b48971162a8
Josh Elser <elserj@apache.org>
2018-09-25 11:00:21 -0400

    Revert "fix related to QE-19650 to accept an empty truststore password file TESTSUITE_FILE=Accumulo"
    This reverts commit 8cfb136b4c2356ff2d1aa4500c3bea9e26b5848f.
    
    Change-Id: I595a4556995650d2dab29a93e2128bb81d346c79

--- server/monitor/src/main/java/org/apache/accumulo/monitor/EmbeddedWebServer.java
+++ server/monitor/src/main/java/org/apache/accumulo/monitor/EmbeddedWebServer.java
@@ -44,7 +44,7 @@ public class EmbeddedWebServer {
     server = new Server();
     final AccumuloConfiguration conf = Monitor.getContext().getConfiguration();
     if (EMPTY.equals(conf.get(Property.MONITOR_SSL_KEYSTORE)) || EMPTY.equals(conf.get(Property.MONITOR_SSL_KEYSTOREPASS))
-        || EMPTY.equals(conf.get(Property.MONITOR_SSL_TRUSTSTORE))) {
+        || EMPTY.equals(conf.get(Property.MONITOR_SSL_TRUSTSTORE)) || EMPTY.equals(conf.get(Property.MONITOR_SSL_TRUSTSTOREPASS))) {
       connector = new ServerConnector(server, new HttpConnectionFactory());
       usingSsl = false;
     } else {
